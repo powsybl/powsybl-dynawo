c22204a078b97b8ee4965a725df59f28ab078f7b
Marcos de Miguel <demiguelm@aia.es>
2019-11-12 06:55:08 +0100

    fix copyright
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- dynawo-simulator/src/main/java/com/powsybl/dynawo/simulator/DynawoXmlConstants.java
+++ dynawo-simulator/src/main/java/com/powsybl/dynawo/simulator/DynawoXmlConstants.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.dynawo.simulator;
 
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
 public final class DynawoXmlConstants {
 
     public static final String DYN_BASE_URI = "http://www.rte-france.com/dynawo";
