34c936d4c6bf4fe7f59ae76da0fc506be82ea434
Marcos de Miguel <demiguelm@aia.es>
2019-12-17 11:08:53 +0100

    precision must be positive
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- dynawo/src/main/groovy/com/powsybl/dynawo/dsl/job/DynawoJobDslLoader.groovy
+++ dynawo/src/main/groovy/com/powsybl/dynawo/dsl/job/DynawoJobDslLoader.groovy
@@ -151,7 +151,7 @@ class DynawoJobDslLoader {
         }
 
         SimulationSpec precision(double precision) {
-            assert precision != null && precision > 0.0
+            assert precision != null && precision >= 0.0
             this.precision = precision
             return this
         }
