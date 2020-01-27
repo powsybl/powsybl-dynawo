97bfa250bcf93d8bd92d85d72369ce06fb2e02e3
Marcos de Miguel <demiguelm@aia.es>
2020-01-27 07:56:04 +0100

    fix coverage
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- dynawo/src/main/java/com/powsybl/dynawo/simulator/json/DynawoSimulationParametersSerializer.java
+++ dynawo/src/main/java/com/powsybl/dynawo/simulator/json/DynawoSimulationParametersSerializer.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.dynawo.simulator.json;
 
 import java.io.IOException;
@@ -13,12 +19,16 @@ import com.powsybl.dynamicsimulation.json.JsonDynamicSimulationParameters.Extens
 import com.powsybl.dynawo.simulator.DynawoSimulationParameters;
 import com.powsybl.dynawo.simulator.DynawoSimulationParameters.Solver;
 
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
 @AutoService(ExtensionSerializer.class)
-public class DynawoSimulationParametersSerializer implements JsonDynamicSimulationParameters.ExtensionSerializer<DynawoSimulationParameters> {
+public class DynawoSimulationParametersSerializer
+    implements JsonDynamicSimulationParameters.ExtensionSerializer<DynawoSimulationParameters> {
 
     @Override
     public String getCategoryName() {
-        return "dynamic-simulation-parameters";
+        return "DynamicSimulation-parameters";
     }
 
     @Override
@@ -65,6 +75,11 @@ public class DynawoSimulationParametersSerializer implements JsonDynamicSimulati
         SerializerProvider serializerProvider)
         throws IOException {
         jsonGenerator.writeStartObject();
+
+        jsonGenerator.writeStringField("solver", dynawoSimulationParameters.getSolver().toString());
+        jsonGenerator.writeNumberField("IDAorder", dynawoSimulationParameters.getIdaOrder());
+        jsonGenerator.writeStringField("dslFile", dynawoSimulationParameters.getDslFilename());
+
         jsonGenerator.writeEndObject();
     }
 
---
+++ dynawo/src/test/java/com/powsybl/dynawo/simulator/json/DynawoSimulationParametersSerializerTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.dynawo.simulator.json;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.dynamicsimulation.DynamicSimulationParameters;
+import com.powsybl.dynamicsimulation.json.JsonDynamicSimulationParameters;
+import com.powsybl.dynawo.simulator.DynawoSimulationParameters;
+import com.powsybl.dynawo.simulator.DynawoSimulationParameters.Solver;
+
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
+public class DynawoSimulationParametersSerializerTest extends AbstractConverterTest {
+
+    @Test
+    public void roundTrip() throws IOException {
+        DynamicSimulationParameters dynamicSimulationParameters = new DynamicSimulationParameters()
+            .setStartTime(0)
+            .setStopTime(1);
+        DynawoSimulationParameters dynawoSimulationParameters = new DynawoSimulationParameters();
+        dynawoSimulationParameters.setSolver(Solver.SIM);
+        dynawoSimulationParameters.setIdaOrder(2);
+        dynawoSimulationParameters.setDslFilename(null);
+        dynamicSimulationParameters.addExtension(DynawoSimulationParameters.class, dynawoSimulationParameters);
+        roundTripTest(dynamicSimulationParameters, JsonDynamicSimulationParameters::write,
+            JsonDynamicSimulationParameters::read, "/DynawoSimulationParameters.json");
+    }
+}
