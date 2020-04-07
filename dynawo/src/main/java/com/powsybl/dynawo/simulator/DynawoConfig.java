449ddaf5273827440448efab354908e2aacab620
Marcos de Miguel <demiguelm@aia.es>
2020-04-07 09:39:28 +0200

    Required ModuleConfig for dynawo configuration
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ dynawo/src/main/java/com/powsybl/dynawo/simulator/DynawoConfig.java
@@ -0,0 +1,39 @@
+package com.powsybl.dynawo.simulator;
+
+import java.util.Objects;
+
+import com.powsybl.commons.config.ModuleConfig;
+import com.powsybl.commons.config.PlatformConfig;
+
+public class DynawoConfig {
+
+    public static DynawoConfig load() {
+        return load(PlatformConfig.defaultConfig());
+    }
+
+    public static DynawoConfig load(PlatformConfig platformConfig) {
+        ModuleConfig config = platformConfig.getModuleConfig("dynawo");
+        String homeDir = config.getStringProperty("homeDir");
+        boolean debug = config.getBooleanProperty("debug", DEBUG_DEFAULT);
+
+        return new DynawoConfig(homeDir, debug);
+    }
+
+    public DynawoConfig(String homeDir, boolean debug) {
+        this.homeDir = Objects.requireNonNull(homeDir);
+        this.debug = debug;
+    }
+
+    public String getHomeDir() {
+        return homeDir;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    private final String homeDir;
+    private final boolean debug;
+
+    private static final boolean DEBUG_DEFAULT = false;
+}
---
+++ dynawo/src/test/java/com/powsybl/dynawo/simulator/DynawoConfigTest.java
@@ -0,0 +1,50 @@
+package com.powsybl.dynawo.simulator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.powsybl.commons.config.InMemoryPlatformConfig;
+import com.powsybl.commons.config.MapModuleConfig;
+
+public class DynawoConfigTest {
+
+    private InMemoryPlatformConfig platformConfig;
+    private FileSystem fileSystem;
+
+    @Before
+    public void setUp() {
+        fileSystem = Jimfs.newFileSystem(Configuration.unix());
+        platformConfig = new InMemoryPlatformConfig(fileSystem);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        fileSystem.close();
+    }
+
+    private void checkValues(DynawoConfig config, String homeDir, boolean debug) {
+        assertEquals(config.getHomeDir(), homeDir);
+        assertEquals(config.isDebug(), debug);
+    }
+
+    @Test
+    public void checkConfig() {
+        String homeDir = "homeDir";
+        boolean debug = true;
+
+        MapModuleConfig moduleConfig = platformConfig.createModuleConfig("dynawo");
+        moduleConfig.setStringProperty("homeDir", homeDir);
+        moduleConfig.setStringProperty("debug", Boolean.toString(debug));
+        DynawoConfig config = DynawoConfig.load(platformConfig);
+        checkValues(config, homeDir, debug);
+    }
+
+}
