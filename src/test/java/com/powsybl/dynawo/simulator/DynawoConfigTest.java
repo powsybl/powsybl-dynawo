d0a21e44cb0b6d1125177b0af1731918e2400a49
marcosmc <demiguelm@aia.es>
2020-04-07 23:06:30 +0200

    Dynawo configuration (#15)
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ src/test/java/com/powsybl/dynawo/simulator/DynawoConfigTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.dynawo.simulator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.powsybl.commons.config.InMemoryPlatformConfig;
+import com.powsybl.commons.config.MapModuleConfig;
+
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
+public class DynawoConfigTest {
+
+    private InMemoryPlatformConfig platformConfig;
+    private FileSystem fileSystem;
+
+    @Before
+    public void setUp() {
+        fileSystem = Jimfs.newFileSystem(Configuration.unix());
+        platformConfig = new InMemoryPlatformConfig(fileSystem);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        fileSystem.close();
+    }
+
+    @Test
+    public void checkConfig() {
+        String homeDir = "homeDir";
+        boolean debug = true;
+
+        MapModuleConfig moduleConfig = platformConfig.createModuleConfig("dynawo");
+        moduleConfig.setStringProperty("homeDir", homeDir);
+        moduleConfig.setStringProperty("debug", Boolean.toString(debug));
+        DynawoConfig config = DynawoConfig.load(platformConfig);
+        assertEquals(homeDir, config.getHomeDir());
+        assertEquals(debug, config.isDebug());
+    }
+
+}
