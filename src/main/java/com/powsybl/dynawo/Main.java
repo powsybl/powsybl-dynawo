a035f84f9251e15b61731ca814d35eba314c9455
Marcos de Miguel <demiguelm@aia.es>
2020-05-07 11:34:48 +0200

    main class to run dynawo
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ src/main/java/com/powsybl/dynawo/Main.java
@@ -0,0 +1,93 @@
+package com.powsybl.dynawo;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.computation.AbstractExecutionHandler;
+import com.powsybl.computation.Command;
+import com.powsybl.computation.CommandExecution;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.computation.ExecutionEnvironment;
+import com.powsybl.computation.ExecutionReport;
+import com.powsybl.computation.GroupCommandBuilder;
+import com.powsybl.computation.local.LocalComputationConfig;
+import com.powsybl.computation.local.LocalComputationManager;
+import com.powsybl.dynamicsimulation.DynamicSimulationParameters;
+import com.powsybl.dynamicsimulation.DynamicSimulationResult;
+import com.powsybl.dynamicsimulation.DynamicSimulationResultImpl;
+import com.powsybl.dynawo.simulator.DynawoConfig;
+import com.powsybl.dynawo.xml.DynawoConstants;
+import com.powsybl.dynawo.xml.JobsXml;
+import com.powsybl.iidm.network.Network;
+
+public final class Main {
+
+    private static final String DEFAULT_DYNAWO_CMD_NAME = "myEnvDynawo.sh";
+    private static final String WORKING_DIR_PREFIX = "powsybl_dynawo_";
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+
+        Network network = Network.create("test", "test");
+        DynamicSimulationParameters parameters = DynamicSimulationParameters.load();
+
+        DynawoConfig dynawoConfig = DynawoConfig.load();
+        try (ComputationManager computationManager = new LocalComputationManager(LocalComputationConfig.load())) {
+            computationManager.execute(
+                new ExecutionEnvironment(Collections.emptyMap(), WORKING_DIR_PREFIX, dynawoConfig.isDebug()),
+                new AbstractExecutionHandler<DynamicSimulationResult>() {
+
+                    @Override
+                    public List<CommandExecution> before(Path workingDir) {
+                        writeInputFiles(workingDir, network, parameters);
+                        Command cmd = createCommand(workingDir.resolve(DynawoConstants.JOBS_FILENAME));
+                        return Collections.singletonList(new CommandExecution(cmd, 1));
+                    }
+
+                    @Override
+                    public DynamicSimulationResult after(Path workingDir, ExecutionReport report) throws IOException {
+                        super.after(workingDir, report);
+                        return new DynamicSimulationResultImpl(true, "");
+                    }
+
+                    private void writeInputFiles(Path workingDir, Network network, DynamicSimulationParameters parameters) {
+                        DynawoContext context = new DynawoContext(network, parameters);
+                        try {
+                            JobsXml.write(workingDir, context);
+                        } catch (IOException | XMLStreamException e) {
+                            throw new PowsyblException(e.getMessage());
+                        }
+                    }
+
+                    private Command createCommand(Path dynawoJobsFile) {
+                        return new GroupCommandBuilder()
+                            .id("dyn_fs")
+                            .subCommand()
+                            .program(getProgram())
+                            .args("jobs", dynawoJobsFile.toAbsolutePath().toString())
+                            .add()
+                            .build();
+                    }
+
+                    private String getProgram() {
+                        return dynawoConfig.getHomeDir().endsWith("/") ? dynawoConfig.getHomeDir() + DEFAULT_DYNAWO_CMD_NAME
+                            : dynawoConfig.getHomeDir() + "/" + DEFAULT_DYNAWO_CMD_NAME;
+                    }
+                });
+        } catch (IOException e) {
+            LOGGER.error(e.toString());
+        }
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+}
