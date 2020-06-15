120e49df879a969814b7a1e99ed8be2b0af6e0b5
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-06-15 10:54:07 +0200

    Refactor the code to avoid duplication
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- src/main/java/com/powsybl/dynawo/xml/CurvesXml.java
+++ src/main/java/com/powsybl/dynawo/xml/CurvesXml.java
@@ -6,23 +6,19 @@
  */
 package com.powsybl.dynawo.xml;
 
-import static com.powsybl.dynawo.xml.DynawoConstants.CRV_FILENAME;
-import static com.powsybl.dynawo.xml.DynawoXmlConstants.DYN_PREFIX;
-import static com.powsybl.dynawo.xml.DynawoXmlConstants.DYN_URI;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Objects;
-
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
-
+import com.powsybl.commons.exceptions.UncheckedXmlStreamException;
 import com.powsybl.dynamicsimulation.Curve;
 import com.powsybl.dynawo.DynawoContext;
 import com.powsybl.dynawo.simulator.DynawoCurve;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static com.powsybl.dynawo.xml.DynawoConstants.CRV_FILENAME;
+import static com.powsybl.dynawo.xml.DynawoXmlConstants.DYN_URI;
 
 /**
  * @author Marcos de Miguel <demiguelm at aia.es>
@@ -34,32 +30,21 @@ public final class CurvesXml {
 
     public static void write(Path workingDir, DynawoContext context) throws IOException, XMLStreamException {
         Objects.requireNonNull(workingDir);
-        Objects.requireNonNull(context);
         Path file = workingDir.resolve(CRV_FILENAME);
-        try (Writer writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8)) {
-            XMLStreamWriter xmlWriter = XmlStreamWriterFactory.newInstance(writer);
-            try {
-                xmlWriter.writeStartDocument(StandardCharsets.UTF_8.toString(), "1.0");
-                xmlWriter.setPrefix(DYN_PREFIX, DYN_URI);
-                xmlWriter.writeStartElement(DYN_URI, "curvesInput");
-                xmlWriter.writeNamespace(DYN_PREFIX, DYN_URI);
 
-                write(xmlWriter, context);
-
-                xmlWriter.writeEndElement();
-                xmlWriter.writeEndDocument();
-            } finally {
-                xmlWriter.close();
-            }
-        }
+        XmlUtil.write(file, context, "curvesInput", CurvesXml::write);
     }
 
-    private static void write(XMLStreamWriter writer, DynawoContext context) throws XMLStreamException {
-        for (Curve curve : context.getCurves()) {
-            DynawoCurve dynCurve = (DynawoCurve) curve;
-            writer.writeEmptyElement(DYN_URI, "curve");
-            writer.writeAttribute("model", dynCurve.getModelId());
-            writer.writeAttribute("variable", dynCurve.getVariable());
+    private static void write(XMLStreamWriter writer, DynawoContext context) {
+        try {
+            for (Curve curve : context.getCurves()) {
+                DynawoCurve dynCurve = (DynawoCurve) curve;
+                writer.writeEmptyElement(DYN_URI, "curve");
+                writer.writeAttribute("model", dynCurve.getModelId());
+                writer.writeAttribute("variable", dynCurve.getVariable());
+            }
+        } catch (XMLStreamException e) {
+            throw new UncheckedXmlStreamException(e);
         }
     }
 }
