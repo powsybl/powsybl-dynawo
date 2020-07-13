de309f7b2e10467764274ac89f16e4586362370a
Marcos de Miguel <demiguelm@aia.es>
2020-07-13 16:54:03 +0200

    [WIP] first commit, write OmegaRef parameters
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ src/test/java/com/powsybl/dynawo/xml/ParametersXmlTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.dynawo.xml;
+
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.powsybl.dynamicsimulation.DynamicSimulationParameters;
+import com.powsybl.dynawo.DynawoContext;
+import com.powsybl.dynawo.simulator.DynawoSimulationParameters;
+
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
+public class ParametersXmlTest extends DynawoTestUtil {
+
+    @Test
+    public void writeOmegaRef() throws SAXException, IOException, XMLStreamException {
+        DynamicSimulationParameters parameters = DynamicSimulationParameters.load();
+        DynawoSimulationParameters dynawoParameters = DynawoSimulationParameters.load();
+        DynawoContext context = new DynawoContext(network, dynamicModels, curves, parameters, dynawoParameters);
+
+        DydXml.write(tmpDir, context);
+        ParXml.write(tmpDir, context);
+        validate("parameters.xsd", "omega_ref.xml", tmpDir.resolve(DynawoConstants.OMEGAREF_PAR_FILENAME));
+    }
+
+}
