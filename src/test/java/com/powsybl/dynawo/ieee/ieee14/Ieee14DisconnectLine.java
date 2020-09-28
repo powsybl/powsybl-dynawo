9905f0bb9e12fc4f4120c65ecc894fe1ec0584ac
Marcos de Miguel <demiguelm@aia.es>
2020-09-28 17:12:56 +0200

    ieee14 disconnect line unit test
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ src/test/java/com/powsybl/dynawo/ieee/ieee14/Ieee14DisconnectLine.java
@@ -0,0 +1,36 @@
+/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.dynawo.ieee.ieee14;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.powsybl.dynamicsimulation.DynamicSimulationResult;
+import com.powsybl.dynawo.ieee.IeeeTestUtil;
+
+/**
+ * @author Marcos de Miguel <demiguelm at aia.es>
+ */
+public class Ieee14DisconnectLine extends IeeeTestUtil {
+
+    @Before
+    public void setup() throws IOException {
+        super.setup("/ieee14-disconnectline/ieee14-disconnectline.par", "/ieee14-disconnectline/ieee14-disconnectline-network.par", "/ieee14-disconnectline/ieee-solvers.par",
+            "/ieee14-disconnectline/IEEE14.iidm", "/ieee14-disconnectline/curves.groovy", "/ieee14-disconnectline/dynamicModels.groovy",
+            "/ieee14-disconnectline/dynawoParameters.json");
+    }
+
+    @Test
+    public void testSimulation() throws Exception {
+        Ieee14MacroconnectsLocalCommandExecutor commandExecutor = new Ieee14MacroconnectsLocalCommandExecutor(fileSystem, network, getDynawoSimulationParameters(parameters));
+        DynamicSimulationResult result = runSimulation(commandExecutor);
+        assertNotNull(result);
+    }
+}
