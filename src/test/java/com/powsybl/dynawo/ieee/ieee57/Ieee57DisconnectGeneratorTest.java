02d4a800ca7a7fe3a9e087b652a1ff913d6bc1fb
Marcos de Miguel <demiguelm@aia.es>
2020-10-01 17:45:24 +0200

    fix copyright
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- src/test/java/com/powsybl/dynawo/ieee/ieee57/Ieee57DisconnectGeneratorTest.java
+++ src/test/java/com/powsybl/dynawo/ieee/ieee57/Ieee57DisconnectGeneratorTest.java
@@ -1,4 +1,5 @@
-/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,17 +14,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.powsybl.dynamicsimulation.DynamicSimulationResult;
-import com.powsybl.dynawo.ieee.IeeeTestUtil;
+import com.powsybl.dynawo.ieee.AbstractIeeeTest;
 
 /**
  * @author Marcos de Miguel <demiguelm at aia.es>
  */
-public class Ieee57DisconnectGeneratorTest extends IeeeTestUtil {
+public class Ieee57DisconnectGeneratorTest extends AbstractIeeeTest {
 
     @Before
     public void setup() throws IOException {
-        super.setup("/ieee57-disconnectgenerator/ieee57-disconnectgenerator.par", "/ieee57-disconnectgenerator/ieee57-disconnectgenerator-network.par", "/ieee57-disconnectgenerator/ieee-solvers.par",
-            "/ieee57-disconnectgenerator/IEEE57.iidm", "/ieee57-disconnectgenerator/curves.groovy", "/ieee57-disconnectgenerator/dynamicModels.groovy",
+        super.setup(
+            "/ieee57-disconnectgenerator/ieee57-disconnectgenerator.par",
+            "/ieee57-disconnectgenerator/ieee57-disconnectgenerator-network.par",
+            "/ieee57-disconnectgenerator/ieee-solvers.par",
+            "/ieee57-disconnectgenerator/IEEE57.iidm",
+            "/ieee57-disconnectgenerator/dynamicModels.groovy",
+            null,
+            "/ieee57-disconnectgenerator/curves.groovy",
             "/ieee57-disconnectgenerator/dynawoParameters.json");
     }
 
