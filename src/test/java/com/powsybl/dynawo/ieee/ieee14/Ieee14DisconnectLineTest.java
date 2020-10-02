94c5b1f8fe14371dfaf29f48c6beb4e73adffe9e
Marcos de Miguel <demiguelm@aia.es>
2020-10-02 09:33:05 +0200

    Fix copyright and typo
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- src/test/java/com/powsybl/dynawo/ieee/ieee14/Ieee14DisconnectLineTest.java
+++ src/test/java/com/powsybl/dynawo/ieee/ieee14/Ieee14DisconnectLineTest.java
@@ -1,4 +1,5 @@
-/* Copyright (c) 2020, RTE (http://www.rte-france.com)
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,17 +14,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.powsybl.dynamicsimulation.DynamicSimulationResult;
-import com.powsybl.dynawo.ieee.IeeeTestUtil;
+import com.powsybl.dynawo.ieee.AbstractIeeeTest;
 
 /**
  * @author Marcos de Miguel <demiguelm at aia.es>
  */
-public class Ieee14DisconnectLine extends IeeeTestUtil {
+public class Ieee14DisconnectLineTest extends AbstractIeeeTest {
 
     @Before
     public void setup() throws IOException {
-        super.setup("/ieee14-disconnectline/ieee14-disconnectline.par", "/ieee14-disconnectline/ieee14-disconnectline-network.par", "/ieee14-disconnectline/ieee-solvers.par",
-            "/ieee14-disconnectline/IEEE14.iidm", "/ieee14-disconnectline/curves.groovy", "/ieee14-disconnectline/dynamicModels.groovy",
+        super.setup(
+            "/ieee14-disconnectline/ieee14-disconnectline.par",
+            "/ieee14-disconnectline/ieee14-disconnectline-network.par",
+            "/ieee14-disconnectline/ieee-solvers.par",
+            "/ieee14-disconnectline/IEEE14.iidm",
+            "/ieee14-disconnectline/dynamicModels.groovy",
+            "/ieee14-disconnectline/eventModels.groovy",
+            "/ieee14-disconnectline/curves.groovy",
             "/ieee14-disconnectline/dynawoParameters.json");
     }
 
