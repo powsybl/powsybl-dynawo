662944377cb307094630785458e1064b7daaa097
Guillaume Pernin <guillaume.pernin@rte-france.com>
2020-11-27 10:39:56 +0100

    Dynaflow-provider: added a base class for localcommandexecutor
    Signed-off-by: Guillaume Pernin <guillaume.pernin@rte-france.com>

---
+++ src/test/java/com/powsybl/dynaflow/AbstractLocalCommandExecutor.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.dynaflow;
+
+import com.powsybl.computation.local.LocalCommandExecutor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ *
+ * @author Guillaume Pernin <guillaume.pernin at rte-france.com>
+ */
+abstract class AbstractLocalCommandExecutor implements LocalCommandExecutor {
+    @Override
+    public void stop(Path workingDir) {
+
+    }
+
+    @Override
+    public void stopForcibly(Path workingDir) {
+
+    }
+
+    void copyFile(String source, Path target) throws IOException {
+        try (InputStream is = getClass().getResourceAsStream(source)) {
+            Files.copy(is, target, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
