5043266803ece93204f5fa25cf8251d424d55a52
janeg <janeg@aia.es>
2021-03-03 09:01:51 -0800

    Initial design for network update
    Signed-off-by: janeg <janeg@aia.es>

---
+++ commons/src/main/java/DynawoResultsNetworkUpdate.java
@@ -0,0 +1,9 @@
+import com.powsybl.iidm.network.Network;
+
+public final class DynawoResultsNetworkUpdate {
+    private DynawoResultsNetworkUpdate() {
+    }
+
+    public static void update(Network source, Network target) {
+    }
+}
