7e0255c731f065a2f87531e250674d0eda430f5c
Guillem Jané Guasch <65736613+guillemjane-AIA@users.noreply.github.com>
2021-04-12 05:42:53 -0700

    Update Network with DynaFlow results (#77)
    * Introducing DynaFlowContext to manage parameters
    
    * Update network with results coming from Dynaflow-launcher.
    
    Signed-off-by: janeg <janeg@aia.es>
    
    * Update network with results coming from Dynaflow-launcher, correction in dependencies.
    
    Signed-off-by: janeg <janeg@aia.es>
    
    * Corrections in Pom.
    
    Signed-off-by: janeg <janeg@aia.es>
    
    * Update to use powsybl core 4.2.0
    
    Signed-off-by: janeg <janeg@aia.es>
    
    * Correction in parameter name, replacing --iidm with --network
    
    Signed-off-by: janeg <janeg@aia.es>
    
    * remove context class and debug method
    
    Signed-off-by: Luma <zamarrenolm@aia.es>
    
    * use constant for dynawo output filename
    
    Signed-off-by: Luma <zamarrenolm@aia.es>
    
    * fixes after final review
    
    Signed-off-by: Luma <zamarrenolm@aia.es>
    
    Co-authored-by: Luma <zamarrenolm@aia.es>

--- dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowConstants.java
+++ dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowConstants.java
@@ -18,6 +18,8 @@ public final class DynaFlowConstants {
 
     public static final String VERSION = "0.1";
 
+    public static final String OUTPUT_IIDM_FILENAME = "outputIIDM.xml";
+
     private DynaFlowConstants() {
     }
 
