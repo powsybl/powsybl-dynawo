79b5807dd400dffad7d646f5ed33324204a26d86
Marcos de Miguel <demiguelm@aia.es>
2021-05-03 13:16:28 +0200

    remove unused import
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- dynawaltz/src/test/java/com/powsybl/dynawaltz/DynaWaltzProviderTest.java
+++ dynawaltz/src/test/java/com/powsybl/dynawaltz/DynaWaltzProviderTest.java
@@ -14,7 +14,6 @@ import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ForkJoinPool;
 
 import com.powsybl.computation.local.LocalCommandExecutor;
