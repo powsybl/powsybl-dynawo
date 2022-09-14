ea2a05c94fd1d18f78ac0bad06cc80e8bc61699c
VEDELAGO MIORA <miora.ralambotiana@rte-france.com>
2022-09-14 13:36:48 +0200

    Bump core to 4.10.0
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

--- commons/src/test/java/com/powsybl/dynawo/commons/DynawoResultsNetworkUpdateTest.java
+++ commons/src/test/java/com/powsybl/dynawo/commons/DynawoResultsNetworkUpdateTest.java
@@ -7,11 +7,12 @@
 package com.powsybl.dynawo.commons;
 
 import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.commons.ComparisonUtils;
 import com.powsybl.commons.datasource.ResourceDataSource;
 import com.powsybl.commons.datasource.ResourceSet;
-import com.powsybl.iidm.export.ExportOptions;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.xml.ExportOptions;
 import com.powsybl.iidm.xml.NetworkXml;
 import org.junit.Test;
 
@@ -109,7 +110,7 @@ public class DynawoResultsNetworkUpdateTest extends AbstractConverterTest {
         NetworkXml.write(expected, pexpected);
         actual.setCaseDate(expected.getCaseDate());
         NetworkXml.write(actual, pactual);
-        compareXml(Files.newInputStream(pexpected), Files.newInputStream(pactual));
+        ComparisonUtils.compareXml(Files.newInputStream(pexpected), Files.newInputStream(pactual));
     }
 
     private static Network createTestCaseBusBranch() {
