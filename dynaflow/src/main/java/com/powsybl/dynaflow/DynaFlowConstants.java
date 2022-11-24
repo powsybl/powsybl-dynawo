97be6560a231e5ce02f7e5c3a06ffa5ddc80b2a8
BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>
2022-11-24 14:30:11 +0100

    Correction on current DynaFlow version currently used
    Signed-off-by: BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>

--- dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowConstants.java
+++ dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowConstants.java
@@ -22,7 +22,7 @@ public final class DynaFlowConstants {
 
     public static final String IIDM_FILENAME = "network.xiidm";
 
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.3.1";
 
     public static final String OUTPUT_IIDM_FILENAME = "outputIIDM.xml";
 
