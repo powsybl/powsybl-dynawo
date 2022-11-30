e5f60fc83dd3adccdafec77a365e81734d316e6a
BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>
2022-11-30 13:41:57 +0100

    Forgot to test checkstyle...
    Signed-off-by: BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>

--- dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowUtil.java
+++ dynaflow/src/main/java/com/powsybl/dynaflow/DynaFlowUtil.java
@@ -15,7 +15,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
