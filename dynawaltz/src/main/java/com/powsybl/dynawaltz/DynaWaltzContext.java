ae8912ed91f83328d5a100259215e7138ef7b708
BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>
2022-12-02 15:04:00 +0100

    getStaticIdBlackBoxModelMap: filter blackboxmodels that hold a staticid
    Signed-off-by: BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/DynaWaltzContext.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/DynaWaltzContext.java
@@ -111,15 +111,15 @@ public class DynaWaltzContext {
         }
     }
 
-    public Map<String, BlackBoxModelWithStaticRef> getStaticIdBlackBoxModelMap() {
+    public Map<String, BlackBoxModelWithStaticId> getStaticIdBlackBoxModelMap() {
         return getBlackBoxModelStream()
-                .filter(BlackBoxModelWithStaticRef.class::isInstance)
-                .map(BlackBoxModelWithStaticRef.class::cast)
+                .filter(BlackBoxModelWithStaticId.class::isInstance)
+                .map(BlackBoxModelWithStaticId.class::cast)
                 .filter(blackBoxModel -> !blackBoxModel.getStaticId().isEmpty())
-                .collect(Collectors.toMap(BlackBoxModelWithStaticRef::getStaticId, Function.identity(), this::mergeDuplicateStaticId, LinkedHashMap::new));
+                .collect(Collectors.toMap(BlackBoxModelWithStaticId::getStaticId, Function.identity(), this::mergeDuplicateStaticId, LinkedHashMap::new));
     }
 
-    private BlackBoxModelWithStaticRef mergeDuplicateStaticId(BlackBoxModelWithStaticRef bbm1, BlackBoxModelWithStaticRef bbm2) {
+    private BlackBoxModelWithStaticId mergeDuplicateStaticId(BlackBoxModelWithStaticId bbm1, BlackBoxModelWithStaticId bbm2) {
         throw new AssertionError("Duplicate staticId " + bbm1.getStaticId());
     }
 
