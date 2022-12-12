29fa711d709c00164428d06d99fc16a931b6f4a8
BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>
2022-12-12 15:33:43 +0100

    Simplify test context
    Signed-off-by: BAUDRIER Dimitri <dimitri.baudrier@rte-france.com>

--- dynawaltz/src/test/java/com/powsybl/dynawaltz/dynamicmodels/MacroConnectorTest.java
+++ dynawaltz/src/test/java/com/powsybl/dynawaltz/dynamicmodels/MacroConnectorTest.java
@@ -1,62 +1,21 @@
 package com.powsybl.dynawaltz.dynamicmodels;
 
-import com.powsybl.dynawaltz.DynaWaltzContext;
 import com.powsybl.dynawaltz.dynamicmodels.utils.MacroConnector;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
 
 public class MacroConnectorTest {
-    class DummyBlackBox extends AbstractBlackBoxModelWithParameterId {
-
-        AtomicInteger atomicInteger = new AtomicInteger();
-
-        public DummyBlackBox() {
-            super("parameterId");
-        }
-
-        @Override
-        public String getLib() {
-            return "lib" + atomicInteger.incrementAndGet();
-        }
-
-        @Override
-        public List<Pair<String, String>> getAttributesConnectTo() {
-            return null;
-        }
-
-        @Override
-        public void write(XMLStreamWriter writer, DynaWaltzContext context) throws XMLStreamException {
-
-        }
-
-        @Override
-        public void writeMacroConnect(XMLStreamWriter writer, DynaWaltzContext context, MacroConnector macroConnector, BlackBoxModel connected) throws XMLStreamException {
-
-        }
-
-        @Override
-        public List<Pair<String, String>> getVarsConnect(BlackBoxModel connected) {
-            return null;
-        }
-
-        @Override
-        public List<BlackBoxModel> getModelsConnectedTo(DynaWaltzContext dynaWaltzContext) {
-            return null;
-        }
-    }
 
     @Test
     public void compareTwoMacroConnectorsEqual() {
-        DummyBlackBox dumdum1 = new DummyBlackBox();
-        DummyBlackBox dumdum2 = new DummyBlackBox();
+        BlackBoxModel dumdum1 = mock(BlackBoxModel.class);
+        BlackBoxModel dumdum2 = mock(BlackBoxModel.class);
 
         MacroConnector connector1 = new MacroConnector(dumdum1, dumdum2, List.of(Pair.of("pair1", "pair2")));
         MacroConnector connector2 = new MacroConnector(dumdum1, dumdum2, List.of(Pair.of("pair1", "pair2")));
@@ -65,28 +24,28 @@ public class MacroConnectorTest {
 
     @Test
     public void compareTwoMacroConnectorsSameLib1DifferentLib2() {
-        DummyBlackBox dumdum1 = new DummyBlackBox();
-        DummyBlackBox dumdum2 = new DummyBlackBox();
+        BlackBoxModel dumdum1 = mock(BlackBoxModel.class);
+        BlackBoxModel dumdum2 = mock(BlackBoxModel.class);
 
         MacroConnector connector1 = new MacroConnector(dumdum1, dumdum2, List.of(Pair.of("pair1", "pair2")));
-        MacroConnector connector2 = new MacroConnector(dumdum1, new DummyBlackBox(), List.of(Pair.of("pair1", "pair2")));
+        MacroConnector connector2 = new MacroConnector(dumdum1, mock(BlackBoxModel.class), List.of(Pair.of("pair1", "pair2")));
         assertNotEquals(connector1, connector2);
     }
 
     @Test
     public void compareTwoMacroConnectorsSameLib2DifferentLib1() {
-        DummyBlackBox dumdum1 = new DummyBlackBox();
-        DummyBlackBox dumdum2 = new DummyBlackBox();
+        BlackBoxModel dumdum1 = mock(BlackBoxModel.class);
+        BlackBoxModel dumdum2 = mock(BlackBoxModel.class);
 
         MacroConnector connector1 = new MacroConnector(dumdum1, dumdum2, List.of(Pair.of("pair1", "pair2")));
-        MacroConnector connector2 = new MacroConnector(new DummyBlackBox(), dumdum2, List.of(Pair.of("pair1", "pair2")));
+        MacroConnector connector2 = new MacroConnector(mock(BlackBoxModel.class), dumdum2, List.of(Pair.of("pair1", "pair2")));
         assertNotEquals(connector1, connector2);
     }
 
     @Test
     public void compareTwoMacroConnectorsInvertedLibs() {
-        DummyBlackBox dumdum1 = new DummyBlackBox();
-        DummyBlackBox dumdum2 = new DummyBlackBox();
+        BlackBoxModel dumdum1 = mock(BlackBoxModel.class);
+        BlackBoxModel dumdum2 = mock(BlackBoxModel.class);
 
         MacroConnector connector1 = new MacroConnector(dumdum1, dumdum2, List.of(Pair.of("pair1", "pair2")));
         MacroConnector connector2 = new MacroConnector(dumdum2, dumdum1, List.of(Pair.of("pair1", "pair2")));
