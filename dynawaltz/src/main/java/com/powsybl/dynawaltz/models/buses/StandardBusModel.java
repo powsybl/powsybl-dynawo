70575e9da4f04d8b38fe4b6cb1ce101d70c862f9
Florian Dupuy <florian.dupuy@rte-france.com>
2022-12-19 17:39:17 +0100

    Fix licenses headers
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/buses/StandardBusModel.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/buses/StandardBusModel.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.buses;
 
 import com.powsybl.commons.PowsyblException;
@@ -20,6 +27,9 @@ import java.util.List;
 
 import static com.powsybl.dynawaltz.xml.DynaWaltzXmlConstants.DYN_URI;
 
+/**
+ * @author Dimitri Baudrier <dimitri.baudrier at rte-france.com>
+ */
 public class StandardBusModel extends AbstractBlackBoxModel implements BusModel {
 
     public StandardBusModel(String dynamicModelId, String staticId, String parameterSetId) {
