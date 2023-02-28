2c42cc9f06ac47fe4e78ea56cf29222100f6789d
lisrte <laurent.issertial@rte-france.com>
2023-02-28 14:44:58 +0100

    Add copyright/author comment
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/utils/BusUtils.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/utils/BusUtils.java
@@ -1,9 +1,19 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.utils;
 
 import com.powsybl.iidm.network.Generator;
 import com.powsybl.iidm.network.Load;
 import com.powsybl.iidm.network.Terminal;
 
+/**
+ * @author Laurent Issertial <laurent.issertial at rte-france.com>
+ */
 public final class BusUtils {
 
     private BusUtils() {
