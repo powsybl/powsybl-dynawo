941ab220fb77ea50bd4dbcd3039802777a920959
lisrte <laurent.issertial@rte-france.com>
2023-04-18 17:02:02 +0200

    Fix copyrights & UT
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/automatons/phaseshifters/PhaseShifterPAutomaton.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/automatons/phaseshifters/PhaseShifterPAutomaton.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.automatons.phaseshifters;
 
 import com.powsybl.dynawaltz.models.VarConnection;
@@ -7,6 +14,9 @@ import com.powsybl.iidm.network.TwoWindingsTransformer;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author Laurent Issertial <laurent.issertial at rte-france.com>
+ */
 public class PhaseShifterPAutomaton extends AbstractPhaseShifterAutomaton {
 
     public PhaseShifterPAutomaton(String dynamicModelId, TwoWindingsTransformer transformer, String parameterSetId) {
