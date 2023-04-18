77a75669eb0816fb42a166f67214a0ea0b30c5b2
lisrte <laurent.issertial@rte-france.com>
2023-04-18 16:33:33 +0200

    Fix copyrights and imports
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz-dsl/src/main/groovy/com/powsybl/dynawaltz/dsl/events/EventDisconnectionGroovyExtension.groovy
+++ dynawaltz-dsl/src/main/groovy/com/powsybl/dynawaltz/dsl/events/EventDisconnectionGroovyExtension.groovy
@@ -1,8 +1,9 @@
 /**
- * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 package com.powsybl.dynawaltz.dsl.events
 
@@ -17,14 +18,10 @@ import com.powsybl.dynawaltz.models.events.EventSetPointBoolean
 import com.powsybl.iidm.network.Branch
 import com.powsybl.iidm.network.Identifiable
 import com.powsybl.iidm.network.IdentifiableType
-import com.powsybl.iidm.network.Line
 import com.powsybl.iidm.network.Network
 
 /**
- * An implementation of {@link EventModelGroovyExtension} that adds the <pre>EventQuadripoleDisconnection</pre> keyword to the DSL
- *
  * @author Laurent Issertial <laurent.issertial at rte-france.com>
- * @author Marcos de Miguel <demiguelm at aia.es>
  */
 @AutoService(EventModelGroovyExtension.class)
 class EventDisconnectionGroovyExtension extends AbstractPureDynamicGroovyExtension<EventModel> implements EventModelGroovyExtension {
