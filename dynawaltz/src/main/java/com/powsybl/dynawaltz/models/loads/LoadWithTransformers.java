1d7f2002ac69125a3013d42f632bfbb34ffc87e2
lisrte <laurent.issertial@rte-france.com>
2023-04-18 16:39:54 +0200

    Fix copyrights
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/loads/LoadWithTransformers.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/loads/LoadWithTransformers.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.loads;
 
 import com.powsybl.dynawaltz.models.Model;
@@ -6,6 +13,9 @@ import com.powsybl.dynawaltz.models.VarConnection;
 
 import java.util.List;
 
+/**
+ * @author Laurent Issertial <laurent.issertial at rte-france.com>
+ */
 public interface LoadWithTransformers extends Model {
 
     List<VarConnection> getTapChangerVarConnections(TransformerSide side);
