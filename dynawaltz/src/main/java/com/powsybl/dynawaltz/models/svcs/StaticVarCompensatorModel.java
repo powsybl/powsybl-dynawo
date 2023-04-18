178a32ac24c5984bee968569fb74e2cb19cb9a35
lisrte <laurent.issertial@rte-france.com>
2023-04-18 16:22:32 +0200

    Add missing copyrights
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/svcs/StaticVarCompensatorModel.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/svcs/StaticVarCompensatorModel.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.svcs;
 
 import com.powsybl.dynawaltz.DynaWaltzContext;
@@ -12,6 +19,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * @author Laurent Issertial <laurent.issertial at rte-france.com>
+ */
 public class StaticVarCompensatorModel extends AbstractEquipmentBlackBoxModel<StaticVarCompensator> {
 
     private static final List<VarMapping> VAR_MAPPING = Arrays.asList(
