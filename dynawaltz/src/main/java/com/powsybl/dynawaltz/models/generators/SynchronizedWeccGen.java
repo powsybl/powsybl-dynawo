15b2d6031750243d2dcfb2561d5cb18875a398fe
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-06-20 16:17:55 +0200

    Add licence header
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/models/generators/SynchronizedWeccGen.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/models/generators/SynchronizedWeccGen.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dynawaltz.models.generators;
 
 import com.powsybl.dynawaltz.models.frequencysynchronizers.FrequencySynchronizedModel;
@@ -5,6 +12,9 @@ import com.powsybl.iidm.network.Generator;
 
 import static com.powsybl.dynawaltz.models.utils.BusUtils.getConnectableBusStaticId;
 
+/**
+ * @author Laurent Issertial <laurent.issertial at rte-france.com>
+ */
 public class SynchronizedWeccGen extends WeccGen implements FrequencySynchronizedModel {
 
     public SynchronizedWeccGen(String dynamicModelId, Generator generator, String parameterSetId, String weccLib, String weccPrefix) {
