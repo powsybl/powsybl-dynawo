761b093ce6819892ec4835854e8e877b0f8597dd
Florian Dupuy <florian.dupuy@rte-france.com>
2023-11-23 13:40:45 +0100

    Fix checkstyle
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- dynawo-integration-tests/src/test/java/com/powsybl/dynawo/it/DynaWaltzTest.java
+++ dynawo-integration-tests/src/test/java/com/powsybl/dynawo/it/DynaWaltzTest.java
@@ -31,7 +31,6 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.powsybl.commons.reporter.Reporter.NO_OP;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
