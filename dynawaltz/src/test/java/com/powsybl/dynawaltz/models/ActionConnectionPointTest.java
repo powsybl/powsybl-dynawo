399885746a2ab01e323e9d44b16e704d4542490b
lisrte <laurent.issertial@rte-france.com>
2024-04-22 16:16:29 +0200

    Add test
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

---
+++ dynawaltz/src/test/java/com/powsybl/dynawaltz/models/ActionConnectionPointTest.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.dynawaltz.models;
+
+import com.powsybl.dynawaltz.builders.BuildersUtil;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+/**
+ * @author Laurent Issertial {@literal <laurent.issertial at rte-france.com>}
+ */
+class ActionConnectionPointTest {
+
+    @Test
+    void voltageOffBus() {
+        Network network = EurostagTutorialExample1Factory.create();
+        network.getGenerator("GEN").disconnect();
+        assertNull(BuildersUtil.getActionConnectionPoint(network, "NGEN"));
+    }
+}
