753ba127fda8da6e22d4408f82334f8bbe5b56a7
lisrte <laurent.issertial@rte-france.com>
2024-06-13 15:38:11 +0200

    Code smell fix
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawaltz/src/main/java/com/powsybl/dynawaltz/suppliers/curves/CurvesJsonDeserializer.java
+++ dynawaltz/src/main/java/com/powsybl/dynawaltz/suppliers/curves/CurvesJsonDeserializer.java
@@ -22,7 +22,7 @@ import java.util.function.Supplier;
  */
 public class CurvesJsonDeserializer extends StdDeserializer<List<DynawoCurvesBuilder>> {
 
-    private final Supplier<DynawoCurvesBuilder> builderConstructor;
+    private transient final Supplier<DynawoCurvesBuilder> builderConstructor;
 
     public CurvesJsonDeserializer(Supplier<DynawoCurvesBuilder> builderConstructor) {
         super(List.class);
--- dynawaltz/src/test/java/com/powsybl/dynawaltz/builders/CurvesBuilderTest.java
+++ dynawaltz/src/test/java/com/powsybl/dynawaltz/builders/CurvesBuilderTest.java
@@ -19,7 +19,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CurvesBuilderTest {
+class CurvesBuilderTest {
 
     private ReportNode reporter;
 
