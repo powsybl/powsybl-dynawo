fde10f2afc8893838843c734266859357dbf382b
Lisrte <laurent.issertial@rte-france.com>
2024-07-02 16:52:24 +0200

    Add deserialization in DynawoModelsSupplier/DynawoEventModelsSupplier (#363)
    * Add deserialization in DynawoModelsSupplier and DynawoEventModelsSupplier through a load static method
    * Add integration test
    * Add unit test
    
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawo-integration-tests/src/test/java/com/powsybl/dynawo/it/DynaWaltzTest.java
+++ dynawo-integration-tests/src/test/java/com/powsybl/dynawo/it/DynaWaltzTest.java
@@ -16,6 +16,8 @@ import com.powsybl.dynawaltz.DynaWaltzConfig;
 import com.powsybl.dynawaltz.DynaWaltzParameters;
 import com.powsybl.dynawaltz.DynaWaltzProvider;
 import com.powsybl.dynawaltz.parameters.ParametersSet;
+import com.powsybl.dynawaltz.suppliers.dynamicmodels.DynawoModelsSupplier;
+import com.powsybl.dynawaltz.suppliers.events.DynawoEventModelsSupplier;
 import com.powsybl.dynawaltz.xml.ParametersXml;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.VariantManagerConstants;
@@ -271,6 +273,34 @@ class DynaWaltzTest extends AbstractDynawoTest {
         assertEquals("time step <= 0.1 s for more than 10 iterations ( DYNSolverCommonFixedTimeStep.cpp:419 )", result.getStatusText());
         assertTrue(result.getTimeLine().isEmpty());
         assertTrue(result.getCurves().isEmpty());
+    }
+
+    @Test
+    void testIeee14DynawoSuppliers() {
+        Network network = Network.read(new ResourceDataSource("IEEE14", new ResourceSet("/ieee14", "IEEE14.iidm")));
+
+        DynamicModelsSupplier dynamicModelsSupplier = DynawoModelsSupplier.load(getResourceAsStream("/ieee14/disconnectline/dynamicModels.json"));
+        EventModelsSupplier eventModelsSupplier = DynawoEventModelsSupplier.load(getResourceAsStream("/ieee14/disconnectline/eventModels.json"));
+
+        List<ParametersSet> modelsParameters = ParametersXml.load(getResourceAsStream("/ieee14/disconnectline/models.par"));
+        ParametersSet networkParameters = ParametersXml.load(getResourceAsStream("/ieee14/disconnectline/network.par"), "8");
+        ParametersSet solverParameters = ParametersXml.load(getResourceAsStream("/ieee14/disconnectline/solvers.par"), "2");
+        dynaWaltzParameters.setModelsParameters(modelsParameters)
+                .setNetworkParameters(networkParameters)
+                .setSolverParameters(solverParameters)
+                .setSolverType(DynaWaltzParameters.SolverType.IDA)
+                .setTimelineExportMode(DynaWaltzParameters.ExportMode.XML);
+
+        DynamicSimulationResult result = provider.run(network, dynamicModelsSupplier, eventModelsSupplier, CurvesSupplier.empty(),
+                        VariantManagerConstants.INITIAL_VARIANT_ID, computationManager, parameters, NO_OP)
+                .join();
+
+        assertEquals(DynamicSimulationResult.Status.SUCCESS, result.getStatus());
+        assertTrue(result.getStatusText().isEmpty());
+        assertEquals(0, result.getCurves().size());
+        List<TimelineEvent> timeLine = result.getTimeLine();
+        assertEquals(11, timeLine.size());
+        checkFirstTimeLineEvent(timeLine.get(0), 0, "_GEN____8_SM", "PMIN : activation");
     }
 
     private void checkFirstTimeLineEvent(TimelineEvent event, double time, String modelName, String message) {
