4b7dd7054c16a035a490c8f320e72d51ec61c738
lisrte <laurent.issertial@rte-france.com>
2025-01-27 16:50:00 +0100

    Fix MarginCalculationParametersTest
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawo-algorithms/dynawo-margin-calculation/src/test/java/com/powsybl/dynawo/margincalculation/margincalculation/MarginCalculationParametersTest.java
+++ dynawo-algorithms/dynawo-margin-calculation/src/test/java/com/powsybl/dynawo/margincalculation/margincalculation/MarginCalculationParametersTest.java
@@ -14,8 +14,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author Laurent Issertial <laurent.issertial at rte-france.com>
@@ -33,21 +32,22 @@ class MarginCalculationParametersTest {
                 .setLoadIncreaseStartTime(loadIncreaseStartTime)
                 .setLoadIncreaseStopTime(loadIncreaseStopTime)
                 .setContingenciesStartTime(contingenciesStartTime);
-        Exception e = assertThrows(IllegalStateException.class, builder::build);
-        assertEquals(message, e.getMessage());
+        assertThatThrownBy(builder::build)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(message);
     }
 
     private static Stream<Arguments> provideMarginCalculationTimes() {
         return Stream.of(
-                Arguments.of(-1, 200, 100, 20, 70, 150, "Start time (-1,00) should be zero or positive"),
-                Arguments.of(10, 5, 100, 20, 70, 150, "Stop time (5,00) should be greater than start time (10,00)"),
-                Arguments.of(10, 200, 5, 20, 70, 150, "Margin calculation start time (5,00) should be between start (10,00) and stop time (200,00)"),
-                Arguments.of(10, 200, 205, 20, 70, 150, "Margin calculation start time (205,00) should be between start (10,00) and stop time (200,00)"),
-                Arguments.of(10, 200, 100, 20, 70, 90, "Contingencies start time (90,00) should be between margin calculation start time (100,00) and stop time (200,00)"),
-                Arguments.of(10, 200, 100, 20, 70, 210, "Contingencies start time (210,00) should be between margin calculation start time (100,00) and stop time (200,00)"),
-                Arguments.of(10, 200, 100, 5, 70, 150, "Load increase start time (5,00) should be greater than start time (10,00)"),
-                Arguments.of(10, 200, 100, 20, 15, 150, "Load increase stop time (15,00) should be between load increase start time (20,00) and margin calculation start time (100,00)"),
-                Arguments.of(10, 200, 100, 20, 110, 150, "Load increase stop time (110,00) should be between load increase start time (20,00) and margin calculation start time (100,00)")
+                Arguments.of(-1, 200, 100, 20, 70, 150, "Start time (%.2f) should be zero or positive".formatted(-1f)),
+                Arguments.of(10, 5, 100, 20, 70, 150, "Stop time (%.2f) should be greater than start time (%.2f)".formatted(5f, 10f)),
+                Arguments.of(10, 200, 5, 20, 70, 150, "Margin calculation start time (%.2f) should be between start (%.2f) and stop time (%.2f)".formatted(5f, 10f, 200f)),
+                Arguments.of(10, 200, 205, 20, 70, 150, "Margin calculation start time (%.2f) should be between start (%.2f) and stop time (%.2f)".formatted(205f, 10f, 200f)),
+                Arguments.of(10, 200, 100, 20, 70, 90, "Contingencies start time (%.2f) should be between margin calculation start time (%.2f) and stop time (%.2f)".formatted(90f, 100f, 200f)),
+                Arguments.of(10, 200, 100, 20, 70, 210, "Contingencies start time (%.2f) should be between margin calculation start time (%.2f) and stop time (%.2f)".formatted(210f, 100f, 200f)),
+                Arguments.of(10, 200, 100, 5, 70, 150, "Load increase start time (%.2f) should be greater than start time (%.2f)".formatted(5f, 10f)),
+                Arguments.of(10, 200, 100, 20, 15, 150, "Load increase stop time (%.2f) should be between load increase start time (%.2f) and margin calculation start time (%.2f)".formatted(15f, 20f, 100f)),
+                Arguments.of(10, 200, 100, 20, 110, 150, "Load increase stop time (%.2f) should be between load increase start time (%.2f) and margin calculation start time (%.2f)".formatted(110f, 20f, 100f))
         );
     }
 }
