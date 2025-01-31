d571ccce281f591b91e68976e5d0a02abe36cadb
lisrte <laurent.issertial@rte-france.com>
2025-01-31 10:47:21 +0100

    Fix tests with isEqualToIgnoringNewLines
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawo-algorithms/dynawo-margin-calculation/src/test/java/com/powsybl/dynawo/margincalculation/margincalculation/tool/MarginCalculationToolTest.java
+++ dynawo-algorithms/dynawo-margin-calculation/src/test/java/com/powsybl/dynawo/margincalculation/margincalculation/tool/MarginCalculationToolTest.java
@@ -234,6 +234,6 @@ class MarginCalculationToolTest extends AbstractToolTest {
     @Override
     protected void assertCommandSuccessful(String[] args, String expectedOut) {
         assertCommandResult(args, CommandLineTools.COMMAND_OK_STATUS, expectedOut, "",
-                (s1, s2) -> assertThat(s1).isEqualToNormalizingNewlines(s2));
+                (s1, s2) -> assertThat(s1).isEqualToIgnoringNewLines(s2));
     }
 }
