df38851f8af90f9bce946d85da91e9d870588922
lisrte <laurent.issertial@rte-france.com>
2025-03-03 17:59:56 +0100

    Update provider fix tests
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

--- dynawo-simulation/src/main/java/com/powsybl/dynawo/models/AbstractBlackBoxModel.java
+++ dynawo-simulation/src/main/java/com/powsybl/dynawo/models/AbstractBlackBoxModel.java
@@ -7,7 +7,6 @@
 package com.powsybl.dynawo.models;
 
 import com.powsybl.dynawo.DynawoSimulationContext;
-import com.powsybl.dynawo.DynawoSimulationParameters;
 import com.powsybl.dynawo.models.macroconnections.MacroConnectAttribute;
 import com.powsybl.dynawo.parameters.ParametersSet;
 
@@ -17,6 +16,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
+
+import static com.powsybl.dynawo.DynawoSimulationConstants.MODELS_PARAMETERS_FILENAME;
 
 /**
  * @author Luma Zamarreño {@literal <zamarrenolm at aia.es>}
@@ -76,7 +77,7 @@ public abstract class AbstractBlackBoxModel implements BlackBoxModel {
 
     @Override
     public String getParFile(DynawoSimulationContext context) {
-        return DynawoSimulationParameters.MODELS_OUTPUT_PARAMETERS_FILE;
+        return MODELS_PARAMETERS_FILENAME;
     }
 
     @Override
