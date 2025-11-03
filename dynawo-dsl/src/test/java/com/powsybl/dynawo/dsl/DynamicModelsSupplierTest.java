aa6834050f7008a6f3b75fde71c3e78c02e16aea
lisrte <laurent.issertial_externe@rte-france.com>
2025-11-03 17:38:51 +0100

    Replace main component with main connected component
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- dynawo-dsl/src/test/java/com/powsybl/dynawo/dsl/DynamicModelsSupplierTest.java
+++ dynawo-dsl/src/test/java/com/powsybl/dynawo/dsl/DynamicModelsSupplierTest.java
@@ -239,9 +239,9 @@ class DynamicModelsSupplierTest extends AbstractModelSupplierTest {
                         + DSL tests
                            + Groovy Dynamic Models Supplier
                               + Model TapChangerBlockingAutomaton ZAB instantiation KO
-                                 None of '[LOAD, Wrong_ID]' values from 'uMeasurements' field where found for energized and in main component equipment type(s) BUS/BUSBAR_SECTION
-                                 'uMeasurements' field value 'NGEN' should be energized and in main component
-                                 None of '[NGEN_NHV1, NGEN]' values from 'uMeasurements' field where found for energized and in main component equipment type(s) BUS/BUSBAR_SECTION
+                                 None of '[LOAD, Wrong_ID]' values from 'uMeasurements' field where found for energized and in main connected component equipment type(s) BUS/BUSBAR_SECTION
+                                 'uMeasurements' field value 'NGEN' should be energized and in main connected component
+                                 None of '[NGEN_NHV1, NGEN]' values from 'uMeasurements' field where found for energized and in main connected component equipment type(s) BUS/BUSBAR_SECTION
                                  'uMeasurements' list is empty
                         """),
                 Arguments.of("/warnings/tapChangerCompatible.groovy", EurostagTutorialExample1Factory.create(),
