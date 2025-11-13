28de77933abe3e8c883f77feb9daf68e7bd31b25
Riad Benradi <riad.benradi_externe@rte-france.com>
2025-11-13 16:53:10 +0100

    review fixes
    Signed-off-by: Riad Benradi <riad.benradi_externe@rte-france.com>

--- dynawo-simulation/src/main/java/com/powsybl/dynawo/models/events/EventReferenceVoltageVariationBuilder.java
+++ dynawo-simulation/src/main/java/com/powsybl/dynawo/models/events/EventReferenceVoltageVariationBuilder.java
@@ -8,17 +8,20 @@
 package com.powsybl.dynawo.models.events;
 
 import com.powsybl.commons.report.ReportNode;
+import com.powsybl.dynawo.builders.BuilderReports;
 import com.powsybl.dynawo.builders.EventModelInfo;
 import com.powsybl.dynawo.builders.ModelInfo;
 import com.powsybl.dynawo.commons.DynawoVersion;
+import com.powsybl.iidm.network.Injection;
 import com.powsybl.iidm.network.Network;
 
 /**
  * @author Riad Benradi {@literal <riad.benradi at rte-france.com>}
  */
-public class EventReferenceVoltageVariationBuilder extends AbstractEventPowerBuilder<EventReferenceVoltageVariationBuilder, EventReferenceVoltageVariation> {
+public class EventReferenceVoltageVariationBuilder extends AbstractEventModelBuilder<Injection<?>, EventReferenceVoltageVariationBuilder> {
 
     private static final EventModelInfo MODEL_INFO = new EventModelInfo("Step", "ReferenceVoltageVariation", "Reference voltage variation on synchronous/synchronized generator");
+    protected Double deltaU;
 
     public static EventReferenceVoltageVariationBuilder of(Network network) {
         return of(network, ReportNode.NO_OP);
@@ -39,13 +42,28 @@ public class EventReferenceVoltageVariationBuilder extends AbstractEventPowerBui
         return MODEL_INFO.version().includes(dynawoVersion) ? MODEL_INFO : null;
     }
 
-    EventReferenceVoltageVariationBuilder(Network network, ReportNode parentReportNode) {
-        super(network, parentReportNode, MODEL_INFO, "deltaU");
+    protected EventReferenceVoltageVariationBuilder(Network network, ReportNode parentReportNode) {
+        super(network, "GENERATOR/LOAD", parentReportNode);
+    }
+
+    @Override
+    protected Injection<?> findEquipment(String staticId) {
+        Injection<?> equipment = network.getLoad(staticId);
+        return equipment != null ? equipment : network.getGenerator(staticId);
     }
 
     public EventReferenceVoltageVariationBuilder deltaU(double deltaU) {
-        this.deltaValue = deltaU;
+        this.deltaU = deltaU;
         return self();
+    }
+
+    @Override
+    protected void checkData() {
+        super.checkData();
+        if (deltaU == null) {
+            BuilderReports.reportFieldNotSet(reportNode, "deltaU");
+            isInstantiable = false;
+        }
     }
 
     @Override
@@ -55,7 +73,7 @@ public class EventReferenceVoltageVariationBuilder extends AbstractEventPowerBui
 
     @Override
     public EventReferenceVoltageVariation build() {
-        return isInstantiable() ? new EventReferenceVoltageVariation(eventId, builderEquipment.getEquipment(), MODEL_INFO, startTime, deltaValue) : null;
+        return isInstantiable() ? new EventReferenceVoltageVariation(eventId, builderEquipment.getEquipment(), MODEL_INFO, startTime, deltaU) : null;
     }
 
     @Override
