a285aa28cfa944e550c9d8c4464683483ad771ae
Riad Benradi <riad.benradi_externe@rte-france.com>
2026-01-19 14:47:52 +0100

    sonar fixes Signed-off-by: Riad Benradi <riad.benradi_externe@rte-france.com>

--- dynawo-simulation/src/main/java/com/powsybl/dynawo/models/automationsystems/phaseshifters/PhaseShifterPAutomationSystem.java
+++ dynawo-simulation/src/main/java/com/powsybl/dynawo/models/automationsystems/phaseshifters/PhaseShifterPAutomationSystem.java
@@ -10,8 +10,6 @@ package com.powsybl.dynawo.models.automationsystems.phaseshifters;
 import com.powsybl.dynawo.builders.ModelConfig;
 import com.powsybl.dynawo.models.ParameterUpdater;
 import com.powsybl.dynawo.models.VarConnection;
-import com.powsybl.dynawo.models.automationsystems.ConnectionStatefulModel;
-import com.powsybl.dynawo.models.macroconnections.MacroConnectionsAdder;
 import com.powsybl.dynawo.models.transformers.TransformerModel;
 import com.powsybl.dynawo.parameters.ParameterType;
 import com.powsybl.iidm.network.TwoWindingsTransformer;
@@ -22,20 +20,10 @@ import java.util.List;
 /**
  * @author Laurent Issertial {@literal <laurent.issertial at rte-france.com>}
  */
-public class PhaseShifterPAutomationSystem extends AbstractPhaseShifterAutomationSystem implements PhaseShifterPModel, ConnectionStatefulModel {
-
-    private ConnectionState connection = null;
+public class PhaseShifterPAutomationSystem extends AbstractPhaseShifterAutomationSystem {
 
     protected PhaseShifterPAutomationSystem(String dynamicModelId, TwoWindingsTransformer transformer, String parameterSetId, ModelConfig modelConfig) {
         super(dynamicModelId, transformer, parameterSetId, modelConfig);
-    }
-
-    @Override
-    public void createMacroConnections(MacroConnectionsAdder adder) {
-        if (connection == null) {
-            super.createMacroConnections(adder);
-            connection = ConnectionState.CONNECTED;
-        }
     }
 
     protected List<VarConnection> getVarConnectionsWith(TransformerModel connected) {
@@ -44,27 +32,6 @@ public class PhaseShifterPAutomationSystem extends AbstractPhaseShifterAutomatio
                 new VarConnection("phaseShifter_PMonitored", connected.getPMonitoredVarName()),
                 new VarConnection("phaseShifter_AutomatonExists", connected.getDisableInternalTapChangerVarName())
         );
-    }
-
-    @Override
-    public ConnectionState getConnectionState() {
-        return connection;
-    }
-
-    @Override
-    public boolean connect(MacroConnectionsAdder adder) {
-        createMacroConnections(adder);
-        return ConnectionState.CONNECTED == getConnectionState();
-    }
-
-    @Override
-    public TwoWindingsTransformer getConnectedTransformer() {
-        return transformer;
-    }
-
-    @Override
-    public String getLockedVarName() {
-        return "phaseShifter_locked";
     }
 
     @Override
