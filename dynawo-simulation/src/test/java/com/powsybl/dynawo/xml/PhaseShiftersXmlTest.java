820ad7ca5c8fbe8db4d79a0e746ab1970a8deef2
Erwann GOASGUEN <erwann.goasguen_externe@rte-france.com>
2026-02-24 16:33:38 +0100

    Check transformer type for phase shifter (#627)
    Signed-off-by: Erwann GOASGUEN <erwann.goasguen_externe@rte-france.com>

--- dynawo-simulation/src/test/java/com/powsybl/dynawo/xml/PhaseShiftersXmlTest.java
+++ dynawo-simulation/src/test/java/com/powsybl/dynawo/xml/PhaseShiftersXmlTest.java
@@ -53,6 +53,10 @@ class PhaseShiftersXmlTest extends AbstractParametrizedDynamicModelXmlTest {
 
     protected void setupNetwork() {
         network = EurostagTutorialExample1Factory.create();
+        network.getTwoWindingsTransformer("NGEN_NHV1").newPhaseTapChanger()
+                .setTapPosition(0)
+                .beginStep().setR(1.0).setX(2.0).setG(3.0).setB(4.0).setAlpha(5.0).setRho(6.0).endStep()
+                .add();
     }
 
     protected void addDynamicModels(Function<Network, BlackBoxModel> phaseShifterConstructor, boolean dynamicTransformer) {
