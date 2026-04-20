254f374013d11eefb649b579839474f82b6d702f
lisrte <laurent.issertial_externe@rte-france.com>
2026-04-20 15:38:13 +0200

    Add category to ModelConfigs
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- dynawo-simulation/src/main/java/com/powsybl/dynawo/models/generators/SignalNGeneratorBuilder.java
+++ dynawo-simulation/src/main/java/com/powsybl/dynawo/models/generators/SignalNGeneratorBuilder.java
@@ -35,12 +35,8 @@ public class SignalNGeneratorBuilder extends AbstractGeneratorBuilder<SignalNGen
     }
 
     public static SignalNGeneratorBuilder of(Network network, String modelName, ReportNode reportNode) {
-        ModelConfig modelConfig = MODEL_CONFIGS.getModelConfig(modelName);
-        if (modelConfig == null) {
-            BuilderReports.reportModelNotFound(reportNode, CATEGORY, modelName);
-            return null;
-        }
-        return new SignalNGeneratorBuilder(network, modelConfig, reportNode);
+        ModelConfig modelConfig = MODEL_CONFIGS.getModelConfig(modelName, reportNode);
+        return modelConfig != null ? new SignalNGeneratorBuilder(network, modelConfig, reportNode) : null;
     }
 
     public static ModelInfo getDefaultModelInfo() {
