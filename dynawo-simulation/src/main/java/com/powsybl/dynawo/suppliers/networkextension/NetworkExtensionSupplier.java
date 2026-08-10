1b87bf8df5818b184f5c139619f1fcad35566cdc
lisrte <laurent.issertial_externe@rte-france.com>
2026-08-10 15:08:01 +0200

    Fix automation system creation Add remaining automation systems
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- dynawo-simulation/src/main/java/com/powsybl/dynawo/suppliers/networkextension/NetworkExtensionSupplier.java
+++ dynawo-simulation/src/main/java/com/powsybl/dynawo/suppliers/networkextension/NetworkExtensionSupplier.java
@@ -13,9 +13,14 @@ import com.powsybl.dynamicsimulation.DynamicModelsSupplier;
 import com.powsybl.dynawo.DynawoSimulationProvider;
 import com.powsybl.dynawo.builders.EquipmentModelBuilder;
 import com.powsybl.dynawo.builders.ModelConfigsHandler;
-import com.powsybl.dynawo.extensions.api.model.DynawoEquipmentModel;
-import com.powsybl.dynawo.extensions.api.model.DynawoUnderVoltageModel;
+import com.powsybl.dynawo.extensions.api.model.*;
+import com.powsybl.dynawo.models.automationsystems.TapChangerAutomationSystemBuilder;
 import com.powsybl.dynawo.models.automationsystems.UnderVoltageAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.overloadmanagments.DynamicOverloadManagementSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.overloadmanagments.DynamicTwoLevelOverloadManagementSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterBlockingIAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterIAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterPAutomationSystemBuilder;
 import com.powsybl.iidm.network.*;
 
 import java.util.ArrayList;
@@ -47,29 +52,20 @@ public class NetworkExtensionSupplier implements DynamicModelsSupplier {
         getModelFromDynawoEquipmentModel(network.getGeneratorStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add,
                 (gen, ma) -> getUvaModel(gen, modelName -> UnderVoltageAutomationSystemBuilder.of(network, modelName, reportNode), ma));
         getModelFromDynawoEquipmentModel(network.getHvdcLineStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
-        getModelFromDynawoEquipmentModel(network.getLineStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
-        getModelFromDynawoEquipmentModel(network.getLoadStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
+        getModelFromDynawoEquipmentModel(network.getLineStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add,
+                (line, ma) -> getOverloadManagementSystemModel(line, modelName -> DynamicOverloadManagementSystemBuilder.of(network, modelName, reportNode), ma),
+                (line, ma) -> getTwoLevelOverloadManagementSystemModel(line, modelName -> DynamicTwoLevelOverloadManagementSystemBuilder.of(network, modelName, reportNode), ma));
+        getModelFromDynawoEquipmentModel(network.getLoadStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add,
+                (load, ma) -> getTapChangerModel(load, modelName -> TapChangerAutomationSystemBuilder.of(network, modelName, reportNode), ma));
         getModelFromDynawoEquipmentModel(network.getStaticVarCompensatorStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
         getModelFromDynawoEquipmentModel(network.getShuntCompensatorStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
-        getModelFromDynawoEquipmentModel(network.getTwoWindingsTransformerStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add);
-
+        getModelFromDynawoEquipmentModel(network.getTwoWindingsTransformerStream(), modelName -> modelConfigsHandler.getEquipmentModelBuilder(network, modelName, reportNode), models::add,
+                (tfo, ma) -> getOverloadManagementSystemModel(tfo, modelName -> DynamicOverloadManagementSystemBuilder.of(network, modelName, reportNode), ma),
+                (tfo, ma) -> getTwoLevelOverloadManagementSystemModel(tfo, modelName -> DynamicTwoLevelOverloadManagementSystemBuilder.of(network, modelName, reportNode), ma),
+                (tfo, ma) -> getPhaseShifterIModel(tfo, modelName -> PhaseShifterIAutomationSystemBuilder.of(network, modelName, reportNode), ma),
+                (tfo, ma) -> getPhaseShifterPModel(tfo, modelName -> PhaseShifterPAutomationSystemBuilder.of(network, modelName, reportNode), ma),
+                (tfo, ma) -> getPhaseShifterBlockingIModel(tfo, modelName -> PhaseShifterBlockingIAutomationSystemBuilder.of(network, modelName, reportNode), ma));
         return models;
-    }
-
-    private <I extends Identifiable<I>> void getModelFromDynawoEquipmentModel(Stream<I> equipments,
-                                                                              Function<String, EquipmentModelBuilder<I, ?>> builderSupplier,
-                                                                              Consumer<DynamicModel> modelAdder) {
-        equipments.forEach(eq -> {
-            DynawoEquipmentModel<I> ext = eq.getExtensionByName(DynawoEquipmentModel.NAME);
-            if (ext != null) {
-                EquipmentModelBuilder<I, ?> builder = builderSupplier.apply(ext.getModelName());
-                if (builder != null) {
-                    modelAdder.accept(builder.equipment(eq)
-                            .parameterSetId(ext.getParameterSetId())
-                            .build());
-                }
-            }
-        });
     }
 
     @SafeVarargs
@@ -85,10 +81,10 @@ public class NetworkExtensionSupplier implements DynamicModelsSupplier {
                     modelAdder.accept(builder.equipment(eq)
                             .parameterSetId(ext.getParameterSetId())
                             .build());
-                    for (BiConsumer<I, Consumer<DynamicModel>> automationSystemModel : automationSystemModels) {
-                        automationSystemModel.accept(eq, modelAdder);
-                    }
                 }
+            }
+            for (BiConsumer<I, Consumer<DynamicModel>> automationSystemModel : automationSystemModels) {
+                automationSystemModel.accept(eq, modelAdder);
             }
         });
     }
@@ -102,6 +98,97 @@ public class NetworkExtensionSupplier implements DynamicModelsSupplier {
                 modelAdder.accept(builder.dynamicModelId(uva.getDynamicModelId())
                         .parameterSetId(uva.getParameterSetId())
                         .generator(gen.getId())
+                        .build());
+            }
+        }
+    }
+
+    private void getTapChangerModel(Load load, Function<String, TapChangerAutomationSystemBuilder> builderSupplier,
+                             Consumer<DynamicModel> modelAdder) {
+        DynawoTapChangerModel tapChanger = load.getExtensionByName(DynawoTapChangerModel.NAME);
+        if (tapChanger != null) {
+            TapChangerAutomationSystemBuilder builder = builderSupplier.apply(tapChanger.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(tapChanger.getDynamicModelId())
+                        .parameterSetId(tapChanger.getParameterSetId())
+                        .staticId(load.getId())
+                        .side(tapChanger.getSide())
+                        .build());
+            }
+        }
+    }
+
+    private void getPhaseShifterIModel(TwoWindingsTransformer tfo, Function<String, PhaseShifterIAutomationSystemBuilder> builderSupplier,
+                                    Consumer<DynamicModel> modelAdder) {
+        DynawoPhaseShifterIModel phaseShifter = tfo.getExtensionByName(DynawoPhaseShifterIModel.NAME);
+        if (phaseShifter != null) {
+            PhaseShifterIAutomationSystemBuilder builder = builderSupplier.apply(phaseShifter.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(phaseShifter.getDynamicModelId())
+                        .parameterSetId(phaseShifter.getParameterSetId())
+                        .transformer(tfo.getId())
+                        .build());
+            }
+        }
+    }
+
+    private void getPhaseShifterPModel(TwoWindingsTransformer tfo, Function<String, PhaseShifterPAutomationSystemBuilder> builderSupplier,
+                                       Consumer<DynamicModel> modelAdder) {
+        DynawoPhaseShifterPModel phaseShifter = tfo.getExtensionByName(DynawoPhaseShifterPModel.NAME);
+        if (phaseShifter != null) {
+            PhaseShifterPAutomationSystemBuilder builder = builderSupplier.apply(phaseShifter.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(phaseShifter.getDynamicModelId())
+                        .parameterSetId(phaseShifter.getParameterSetId())
+                        .transformer(tfo.getId())
+                        .build());
+            }
+        }
+    }
+
+    private void getPhaseShifterBlockingIModel(TwoWindingsTransformer tfo, Function<String, PhaseShifterBlockingIAutomationSystemBuilder> builderSupplier,
+                                       Consumer<DynamicModel> modelAdder) {
+        DynawoPhaseShifterBlockingIModel phaseShifterBlocking = tfo.getExtensionByName(DynawoPhaseShifterBlockingIModel.NAME);
+        if (phaseShifterBlocking != null) {
+            PhaseShifterBlockingIAutomationSystemBuilder builder = builderSupplier.apply(phaseShifterBlocking.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(phaseShifterBlocking.getDynamicModelId())
+                        .parameterSetId(phaseShifterBlocking.getParameterSetId())
+                        .phaseShifterId(phaseShifterBlocking.getPhaseShifterId())
+                        .build());
+            }
+        }
+    }
+
+    private <B extends Branch<B>> void getOverloadManagementSystemModel(B branch, Function<String, DynamicOverloadManagementSystemBuilder> builderSupplier,
+                                    Consumer<DynamicModel> modelAdder) {
+        DynawoOverloadManagementSystemModel<B> oms = branch.getExtensionByName(DynawoOverloadManagementSystemModel.NAME);
+        if (oms != null) {
+            DynamicOverloadManagementSystemBuilder builder = builderSupplier.apply(oms.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(oms.getDynamicModelId())
+                        .parameterSetId(oms.getParameterSetId())
+                        .controlledBranch(branch.getId())
+                        .iMeasurement(oms.getIMeasurement())
+                        .iMeasurementSide(oms.getIMeasurementSide())
+                        .build());
+            }
+        }
+    }
+
+    private <B extends Branch<B>> void getTwoLevelOverloadManagementSystemModel(B branch, Function<String, DynamicTwoLevelOverloadManagementSystemBuilder> builderSupplier,
+                                                                       Consumer<DynamicModel> modelAdder) {
+        DynawoTwoLevelOverloadManagementSystemModel<B> oms = branch.getExtensionByName(DynawoTwoLevelOverloadManagementSystemModel.NAME);
+        if (oms != null) {
+            DynamicTwoLevelOverloadManagementSystemBuilder builder = builderSupplier.apply(oms.getModelName());
+            if (builder != null) {
+                modelAdder.accept(builder.dynamicModelId(oms.getDynamicModelId())
+                        .parameterSetId(oms.getParameterSetId())
+                        .controlledBranch(branch.getId())
+                        .iMeasurement1(oms.getIMeasurement1())
+                        .iMeasurement1Side(oms.getIMeasurement1Side())
+                        .iMeasurement2(oms.getIMeasurement2())
+                        .iMeasurement2Side(oms.getIMeasurement2Side())
                         .build());
             }
         }
--- dynawo-simulation/src/test/java/com/powsybl/dynawo/suppliers/networkextension/NetworkExtensionSupplierTest.java
+++ dynawo-simulation/src/test/java/com/powsybl/dynawo/suppliers/networkextension/NetworkExtensionSupplierTest.java
@@ -8,13 +8,18 @@
 package com.powsybl.dynawo.suppliers.networkextension;
 
 import com.powsybl.dynamicsimulation.DynamicModel;
-import com.powsybl.dynawo.extensions.api.model.DynawoEquipmentModelAdder;
-import com.powsybl.dynawo.extensions.api.model.DynawoUnderVoltageModelAdder;
+import com.powsybl.dynawo.commons.TransformerSide;
+import com.powsybl.dynawo.extensions.api.model.*;
+import com.powsybl.dynawo.models.automationsystems.TapChangerAutomationSystemBuilder;
 import com.powsybl.dynawo.models.automationsystems.UnderVoltageAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.overloadmanagments.DynamicOverloadManagementSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.overloadmanagments.DynamicTwoLevelOverloadManagementSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterBlockingIAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterIAutomationSystemBuilder;
+import com.powsybl.dynawo.models.automationsystems.phaseshifters.PhaseShifterPAutomationSystemBuilder;
 import com.powsybl.dynawo.models.generators.SynchronizedGeneratorBuilder;
-import com.powsybl.dynawo.models.loads.BaseLoadBuilder;
-import com.powsybl.iidm.network.Generator;
-import com.powsybl.iidm.network.Network;
+import com.powsybl.dynawo.models.loads.LoadOneTransformerBuilder;
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import org.junit.jupiter.api.Test;
 
@@ -28,28 +33,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NetworkExtensionSupplierTest {
 
     @Test
-    void testSupplier() {
+    void testSupplierWithGeneratorExtensions() {
         Network network = EurostagTutorialExample1Factory.createWithLFResults();
         Generator generator = network.getGenerator("GEN");
 
-        network.getLoad("LOAD").newExtension(DynawoEquipmentModelAdder.class)
-                .setModelName("LoadAlphaBeta")
-                .setParameterSetId("LAB")
-                .add();
         generator.newExtension(DynawoEquipmentModelAdder.class)
-                .setModelName("GeneratorPV")
-                .setParameterSetId("GPV")
+                .withModelName("GeneratorPV")
+                .withParameterSetId("GPV")
                 .add();
         generator.newExtension(DynawoUnderVoltageModelAdder.class)
-                .setModelName("UnderVoltage")
-                .setDynamicModelId("UVA")
-                .setParameterSetId("UVA")
+                .withModelName("UnderVoltage")
+                .withDynamicModelId("UVA")
+                .withParameterSetId("UVA")
                 .add();
 
-        DynamicModel expectedLoad = BaseLoadBuilder.of(network, "LoadAlphaBeta")
-                .staticId("LOAD")
-                .parameterSetId("LAB")
-                .build();
         DynamicModel expectedGen = SynchronizedGeneratorBuilder.of(network, "GeneratorPV")
                 .staticId("GEN")
                 .parameterSetId("GPV")
@@ -61,9 +58,138 @@ public class NetworkExtensionSupplierTest {
                 .build();
 
         List<DynamicModel> dynamicModels = new NetworkExtensionSupplier().get(network);
-        assertThat(dynamicModels).hasSize(3).satisfiesExactlyInAnyOrder(
-                load -> assertThat(load).usingRecursiveComparison().isEqualTo(expectedLoad),
+        assertThat(dynamicModels).hasSize(2).satisfiesExactlyInAnyOrder(
                 gen -> assertThat(gen).usingRecursiveComparison().isEqualTo(expectedGen),
                 uva -> assertThat(uva).usingRecursiveComparison().isEqualTo(expectedUva));
+    }
+
+    @Test
+    void testSupplierWithLoadExtensions() {
+        Network network = EurostagTutorialExample1Factory.createWithLFResults();
+        Load load = network.getLoad("LOAD");
+
+        load.newExtension(DynawoEquipmentModelAdder.class)
+                .withModelName("LoadOneTransformer")
+                .withParameterSetId("LOT")
+                .add();
+
+        load.newExtension(DynawoTapChangerModelAdder.class)
+                .withModelName("TapChangerAutomationSystem")
+                .withDynamicModelId("TC")
+                .withParameterSetId("TC")
+                .withSide(TransformerSide.NONE)
+                .add();
+
+        DynamicModel expectedLoad = LoadOneTransformerBuilder.of(network, "LoadOneTransformer")
+                .staticId("LOAD")
+                .parameterSetId("LOT")
+                .build();
+        DynamicModel expectedTc = TapChangerAutomationSystemBuilder.of(network)
+                .dynamicModelId("TC")
+                .parameterSetId("TC")
+                .staticId("LOAD")
+                .build();
+
+        List<DynamicModel> dynamicModels = new NetworkExtensionSupplier().get(network);
+        assertThat(dynamicModels).hasSize(2).satisfiesExactlyInAnyOrder(
+                l -> assertThat(l).usingRecursiveComparison().isEqualTo(expectedLoad),
+                tc -> assertThat(tc).usingRecursiveComparison().isEqualTo(expectedTc));
+    }
+
+    @Test
+    void testSupplierWithPhaseShifterExtensions() {
+        Network network = EurostagTutorialExample1Factory.createWithLFResults();
+        network.getTwoWindingsTransformer(EurostagTutorialExample1Factory.NGEN_NHV1).newPhaseTapChanger()
+                .setTapPosition(0)
+                .beginStep().setR(1.0).setX(2.0).setG(3.0).setB(4.0).setAlpha(5.0).setRho(6.0).endStep()
+                .add();
+        TwoWindingsTransformer tfo = network.getTwoWindingsTransformer(EurostagTutorialExample1Factory.NGEN_NHV1);
+
+        tfo.newExtension(DynawoPhaseShifterPModelAdder.class)
+                .withDynamicModelId("PSP")
+                .withParameterSetId("PS")
+                .withModelName("PhaseShifterP")
+                .add();
+
+        tfo.newExtension(DynawoPhaseShifterIModelAdder.class)
+                .withDynamicModelId("PSI")
+                .withParameterSetId("PS")
+                .withModelName("PhaseShifterI")
+                .add();
+
+        tfo.newExtension(DynawoPhaseShifterBlockingIModelAdder.class)
+                .withDynamicModelId("PSB")
+                .withParameterSetId("PSB")
+                .withModelName("PhaseShifterBlockingI")
+                .withPhaseShifterId("PSI")
+                .add();
+
+        DynamicModel expectedPsp = PhaseShifterPAutomationSystemBuilder.of(network)
+                .dynamicModelId("PSP")
+                .parameterSetId("PS")
+                .transformer("NGEN_NHV1")
+                .build();
+        DynamicModel expectedPsi = PhaseShifterIAutomationSystemBuilder.of(network)
+                .dynamicModelId("PSI")
+                .parameterSetId("PS")
+                .transformer("NGEN_NHV1")
+                .build();
+        DynamicModel expectedPsb = PhaseShifterBlockingIAutomationSystemBuilder.of(network)
+                .dynamicModelId("PSB")
+                .parameterSetId("PSB")
+                .phaseShifterId("PSI")
+                .build();
+
+        List<DynamicModel> dynamicModels = new NetworkExtensionSupplier().get(network);
+        assertThat(dynamicModels).hasSize(3).satisfiesExactlyInAnyOrder(
+                psi -> assertThat(psi).usingRecursiveComparison().isEqualTo(expectedPsi),
+                psp -> assertThat(psp).usingRecursiveComparison().isEqualTo(expectedPsp),
+                psb -> assertThat(psb).usingRecursiveComparison().isEqualTo(expectedPsb));
+    }
+
+    @Test
+    void testSupplierWithOverloadManagementExtensions() {
+        Network network = EurostagTutorialExample1Factory.createWithLFResults();
+        Line line = network.getLine("NHV1_NHV2_1");
+
+        DynawoOverloadManagementSystemModelAdder<Line> adder = line.newExtension(DynawoOverloadManagementSystemModelAdder.class);
+        adder.withDynamicModelId("BBM_CLA")
+                .withParameterSetId("cla")
+                .withModelName("OverloadManagementSystem")
+                .withIMeasurement("NHV2_NLOAD")
+                .withIMeasurementSide(TwoSides.ONE)
+                .add();
+
+        DynawoTwoLevelOverloadManagementSystemModelAdder<Line> adderTL = line.newExtension(DynawoTwoLevelOverloadManagementSystemModelAdder.class);
+        adderTL.withDynamicModelId("BBM_CLA_TWO_LEVELS")
+                .withParameterSetId("cla")
+                .withModelName("TwoLevelOverloadManagementSystem")
+                .withIMeasurement1("NHV2_NLOAD")
+                .withIMeasurement1Side(TwoSides.ONE)
+                .withIMeasurement2("NGEN_NHV1")
+                .withIMeasurement2Side(TwoSides.TWO)
+                .add();
+
+        DynamicModel expectedOms = DynamicOverloadManagementSystemBuilder.of(network, "OverloadManagementSystem")
+                .dynamicModelId("BBM_CLA")
+                .parameterSetId("cla")
+                .controlledBranch("NHV1_NHV2_1")
+                .iMeasurement("NHV2_NLOAD")
+                .iMeasurementSide(TwoSides.ONE)
+                .build();
+        DynamicModel expectedTlOms = DynamicTwoLevelOverloadManagementSystemBuilder.of(network, "TwoLevelOverloadManagementSystem")
+                .dynamicModelId("BBM_CLA_TWO_LEVELS")
+                .parameterSetId("cla")
+                .controlledBranch("NHV1_NHV2_1")
+                .iMeasurement1("NHV2_NLOAD")
+                .iMeasurement1Side(TwoSides.ONE)
+                .iMeasurement2("NGEN_NHV1")
+                .iMeasurement2Side(TwoSides.TWO)
+                .build();
+
+        List<DynamicModel> dynamicModels = new NetworkExtensionSupplier().get(network);
+        assertThat(dynamicModels).hasSize(2).satisfiesExactlyInAnyOrder(
+                oms -> assertThat(oms).usingRecursiveComparison().isEqualTo(expectedOms),
+                tloms -> assertThat(tloms).usingRecursiveComparison().isEqualTo(expectedTlOms));
     }
 }
